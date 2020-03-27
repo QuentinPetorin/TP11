216a04ac0ee6be889be03ac4aa044594e8b87fc8
Quentin Petorin <*quentin.petorin@isen-ouest.yncrea.fr*>
2020-03-27 15:59:05 +0100

    commit

--- Ex5.py
+++ Ex5.py
@@ -5,57 +5,63 @@ class Matrice:
     def __init__(self,data):
         self.__data = data
 
+
     def getData(self):
         return self.__data
 
 
 
-    def __add__(self,obj): #M1
+    def __add__(self,obj): #M3
         return (self.__data + obj.__data)
 
 
 
 
-    def __iadd__(self,obj): #M2
-        return (self.__data += obj.__data)
+    def __iadd__(self,data2): #M4
+        self.__data += data2.__data
+        return self.__data
 
 
 
 
-    def __sub__(self,obj): #M3
+    def __sub__(self,obj): #M5
         return (self.__data - obj.__data)
 
 
 
 
-   # def __isub__(self,obj): #M4
+    def __isub__(self,data2): #M6
+        self.__data -= self.__data2
+        return self.__data
 
 
 
 
-   # def __mul__(self, obj): #M4
+    def __mul__(self, obj): #M7
+        return (self.__data * obj.__data)
 
 
 
 
 
-    #def __imul__(self,obj): #M5
+    def __imul__(self,obj): #M8
+        return self.__data
 
 
 
 
-    def __lt__(self,obj): #M6
+    def __lt__(self,obj): #M9
         return (self.__data < obj.__data)
 
 
 
 
-    def __gt__(self,obj): #M7
+    def __gt__(self,obj): #M10
         return (self.__data > obj.__data)
 
 
-    def __len__(self,obj):
-        return len(obj)
+    def __len__(self): #M11
+        return len(self.__data)
 
 
 
@@ -65,14 +71,27 @@ if __name__ == '__main__':
 
     data1 = np.array([[1,2],[3,4]])
     data2 = np.array([[2,2],[1,2]])
+
     Mat1 = Matrice(data1)
     Mat2 = Matrice(data2)
     Mat3 = Mat1 + Mat2
-    Mat4 =
-    Mat5 =
-    Mat6 =
-    Mat7 =
-    Mat8 =
-    Mat9 =
+ #   print(Mat1 += Mat2)
+    Mat5 = Mat1 - Mat2
+ #   Mat6 =
+ #   Mat7 = Mat1 * Mat2
+ #   Mat8 =
+    Mat9 = Mat1 < Mat2
+    Mat10 = Mat1 > Mat2
+    Mat11 = len(Mat1)
 
     print(Mat3)
+
+    print(Mat5)
+#    print(Mat6)
+ #   print(Mat7)
+#    print(Mat8)
+    print(Mat9)
+    print(Mat10)
+    print("longueur:",Mat11)
+
+
